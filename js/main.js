55275b5122934b5055f88acc1e429202cf072e43
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-04-05 16:56:54 -0300

    Button controls for mobile

--- js/main.js
+++ js/main.js
@@ -19,16 +19,26 @@ var paused = false;
 
 document.onkeypress = function (e) {
     e = e || window.event;
-    if (e.keyCode == 13){ // return
+    if ( e.keyCode == 13 ) { // return
         nextQuestion();
-    } else if (e.keyCode == 32){ // space
+    } else if (e.keyCode == 32) { // space
         flipflopTimer();
     }
 };
 
 function nextQuestion() {
-    if (index >= questions.length){
+    if (index === 0){
+        textArray = document.getElementsByClassName('helptext');
+        for ( var i=0; i<textArray.length; i++ ) {
+            textArray[i].className = textArray[i].className + " hide";
+        }
+        document.getElementById('questionText').innerHTML = 'NEXT QUESTION';
+    }
+    if ( index >= questions.length ) {
         document.getElementById('question').innerHTML = 'fin.'
+        document.getElementById('questionControl').className = document.getElementById('questionControl').className + ' hide';
+        document.getElementById('timerControl').className = document.getElementById('timerControl').className + ' hide';
+        document.getElementById('timer').className = document.getElementById('timer').className + ' hide';
     } else {
         document.getElementById('question').innerHTML = questions[index]
         resetTimer();
@@ -38,27 +48,39 @@ function nextQuestion() {
 
 function resetTimer() {
     timeRemaining = 15;
+    startTimer();
+}
+
+function flipflopTimer() {
+    if ( index === 0 ){
+        return;
+    }
+    if (paused) {
+        startTimer();
+    } else {
+        pauseTimer();
+    }
+}
+
+function startTimer() {
     clearTimeout(timer);
     document.getElementById('timeRemaining').innerHTML = timeRemaining + ' s';
+    document.getElementById('timerText').innerHTML = 'PAUSE TIMER';
     paused = false;
     timer = window.setInterval("decrementTimer();", 1000);
 }
 
+function pauseTimer() {
+    clearTimeout(timer);
+    document.getElementById('timerText').innerHTML = 'RESUME TIMER';
+    paused = true;
+}
+
 function decrementTimer() {
-    if (timeRemaining >= 1) {
+    if ( timeRemaining >= 1 ) {
        timeRemaining = timeRemaining - 1;
     } else {
         timeRemaining = 0;
     }
     document.getElementById('timeRemaining').innerHTML = timeRemaining + ' s'; 
-}
-
-function flipflopTimer() {
-    if (paused) {
-        timer = window.setInterval("decrementTimer();", 1000);
-        paused = false;
-    } else {
-        clearTimeout(timer);
-        paused = true;
-    }
 }
